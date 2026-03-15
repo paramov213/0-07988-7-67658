15ad6d35e5341104b1bc31b75abc54429cd62aa1
paramov213 <epiksou5@gmail.com>
2026-03-15 21:59:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,18 +5,17 @@ const io = require('socket.io')(http);
 const fs = require('fs');
 const path = require('path');
 
-const DB_FILE = path.join(__dirname, 'users.json');
+const DB_FILE = './users.json';
 
-// Загрузка базы данных из файла при старте
+// Загрузка/создание базы данных
 let db = { users: {} };
 if (fs.existsSync(DB_FILE)) {
-    db = JSON.parse(fs.readFileSync(DB_FILE));
+    try {
+        db = JSON.parse(fs.readFileSync(DB_FILE));
+    } catch (e) { console.log("Ошибка чтения БД"); }
 }
 
-// Функция сохранения
-const saveDB = () => {
-    fs.writeFileSync(DB_FILE, JSON.stringify(db, null, 2));
-};
+const saveDB = () => fs.writeFileSync(DB_FILE, JSON.stringify(db, null, 2));
 
 app.use(express.static(__dirname));
 
@@ -24,13 +23,12 @@ io.on('connection', (socket) => {
     // Регистрация
     socket.on('register', (data) => {
         if (db.users[data.login]) {
-            socket.emit('auth-error', 'Логин уже занят');
-        } else {
-            db.users[data.login] = data.password;
-            saveDB();
-            socket.userName = data.login;
-            socket.emit('auth-success', { user: data.login });
+            return socket.emit('auth-error', 'Логин уже занят');
         }
+        db.users[data.login] = data.password;
+        saveDB();
+        socket.userName = data.login;
+        socket.emit('auth-success', { user: data.login });
     });
 
     // Вход
@@ -39,31 +37,27 @@ io.on('connection', (socket) => {
             socket.userName = data.login;
             socket.emit('auth-success', { user: data.login });
         } else {
-            socket.emit('auth-error', 'Неверный логин или пароль');
+            socket.emit('auth-error', 'Ошибка входа');
         }
     });
 
-    // Сообщения
+    // Сообщения и фото
     socket.on('message', (data) => {
         io.emit('message', {
             text: data.text,
             image: data.image,
             user: socket.userName || 'Аноним',
-            time: new Date().toLocaleTimeString()
+            time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
         });
     });
 
-    // WebRTC сигналинг
+    // Сигналинг звонков
     socket.on('call-signal', (data) => {
         socket.broadcast.emit('call-signal', data);
     });
 });
 
-// Для Render: предотвращение "засыпания" (самопрозвон)
-setInterval(() => {
-    http.get(`http://localhost:${process.env.PORT || 3000}`);
-}, 10 * 60 * 1000); 
-
+// Пинг самого себя для Render (каждые 10 минут)
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, '0.0.0.0', () => {
     console.log(`Сервер запущен на порту ${PORT}`);
